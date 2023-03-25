1f40765bd76562dfb2eca5813375f5d1032b56e2
SeoSangwon <126433959+tkd8973@users.noreply.github.com>
2023-03-25 14:38:39 +0900

    SW

---
+++ SW/data.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def read_data():
+    df_apt1 = pd.read_excel('https://github.com/jaiwon880/ML_Projiect/raw/main/Data/%EC%95%84%ED%8C%8C%ED%8A%B8(%EB%A7%A4%EB%A7%A4)_%EC%8B%A4%EA%B1%B0%EB%9E%98%EA%B0%80_1%EC%9B%94.xlsx')
+    df_apt1
+    df_apt2 = pd.read_excel('https://github.com/jaiwon880/ML_Projiect/raw/main/Data/%EC%95%84%ED%8C%8C%ED%8A%B8(%EB%A7%A4%EB%A7%A4)_%EC%8B%A4%EA%B1%B0%EB%9E%98%EA%B0%80_2%EC%9B%94.xlsx')
+    df_apt2
+
+    return df_apt1,df_apt2
---
+++ SW/streamlit_p2.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import data
+
+df_apt1,df_apt2 = data.read_data()
+
+
+# 사이드바 // 메인에서 df의 값을 받아온다. main -> create_df -> side_bar
+def side_bar(df) :
+    s_bar = st.sidebar
+    s_bar.title('지역을 선택해주세요.')
+
+    # area에 df에서 열 중에서 중복된 값들을 제외하고 리스트로 변환
+
+    # 시/도 선택
+    city_list = df['시군구'].apply(lambda x: x.split()[0]).unique()
+    city_choice = s_bar.selectbox('시/도 선택', city_list)
+
+    # 시/군/구 선택
+    gu_list = df['시군구'][df['시군구'].str.contains(city_choice)].apply(lambda x: x.split()[1]).unique()
+    gu_choice = s_bar.selectbox('시/군/구 선택', gu_list)
+
+    # 읍/면/동 선택
+    town_list = df['시군구'][df['시군구'].str.contains(city_choice) & df['시군구'].str.contains(gu_choice)].apply(lambda x: x.split()[2]).unique()
+    town_choice = s_bar.selectbox('읍/면/동 선택', town_list)
+
+    # 리 선택
+    try :
+        village_list = df['시군구'][df['시군구'].str.contains(city_choice) & df['시군구'].str.contains(gu_choice) & df['시군구'].str.contains(town_choice)].apply(lambda x: x.split()[3]).unique()
+        village_choice = s_bar.selectbox('리 선택', village_list)
+        result = df[df['시군구'].str.contains(city_choice) & df['시군구'].str.contains(gu_choice) & df['시군구'].str.contains(town_choice) & df['시군구'].str.contains(village_choice)]
+    except :
+        result = df[df['시군구'].str.contains(city_choice) & df['시군구'].str.contains(gu_choice) & df['시군구'].str.contains(town_choice)]
+    # 선택된 지역의 데이터만 추출
+    
+    # result 데이터프레임의 인덱스를 0->1부터 시작하도록 변경
+    result.index = np.arange(1, len(result) + 1)
+
+    st.dataframe(result)
+    return result
+side_bar(df_apt1)
