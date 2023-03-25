cf45adb5fb2a33e972bb91c55b30ed07b6f01eba
SeoSangwon <126433959+tkd8973@users.noreply.github.com>
2023-03-25 20:16:41 +0900

    test2

--- SW/streamlit_p2.py
+++ SW/streamlit_p2.py
@@ -13,20 +13,20 @@ def side_bar(df1,df2) :
     # area에 df에서 열 중에서 중복된 값들을 제외하고 리스트로 변환
     # 시/도 선택
 
-    city_list = df['시군구'].apply(lambda x: x.split()[0]).unique()
+    city_list = df1['시군구'].apply(lambda x: x.split()[0]).unique()
     city_choice = s_bar.selectbox('시/도 선택', city_list)
 
     # 시/군/구 선택
-    gu_list = df['시군구'][df['시군구'].str.contains(city_choice)].apply(lambda x: x.split()[1]).unique()
+    gu_list = df1['시군구'][df1['시군구'].str.contains(city_choice)].apply(lambda x: x.split()[1]).unique()
     gu_choice = s_bar.selectbox('시/군/구 선택', gu_list)
 
     # 읍/면/동 선택
-    town_list = df['시군구'][df['시군구'].str.contains(city_choice) & df['시군구'].str.contains(gu_choice)].apply(lambda x: x.split()[2]).unique()
+    town_list = df1['시군구'][df1['시군구'].str.contains(city_choice) & df1['시군구'].str.contains(gu_choice)].apply(lambda x: x.split()[2]).unique()
     town_choice = s_bar.selectbox('읍/면/동 선택', town_list)
     
     # 리 선택
     try :
-        village_list = df['시군구'][df['시군구'].str.contains(city_choice) & df['시군구'].str.contains(gu_choice) & df['시군구'].str.contains(town_choice)].apply(lambda x: x.split()[3]).unique()
+        village_list = df1['시군구'][df1['시군구'].str.contains(city_choice) & df1['시군구'].str.contains(gu_choice) & df1['시군구'].str.contains(town_choice)].apply(lambda x: x.split()[3]).unique()
         village_choice = s_bar.selectbox('리 선택', village_list)
         Jan_result = df1[df1['시군구'].str.contains(city_choice) & df1['시군구'].str.contains(gu_choice) & df1['시군구'].str.contains(town_choice) & df1['시군구'].str.contains(village_choice)]
         Feb_result = df2[df2['시군구'].str.contains(city_choice) & df2['시군구'].str.contains(gu_choice) & df2['시군구'].str.contains(town_choice) & df2['시군구'].str.contains(village_choice)]
