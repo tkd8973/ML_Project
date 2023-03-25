2d0beff7cee3a815ba30910f2d3a0c773c0e6fde
Han Marie <71927533+jaiwon880@users.noreply.github.com>
2023-03-25 13:26:12 +0900

    up14

--- web/app.py
+++ web/app.py
@@ -4,8 +4,8 @@ import pandas as pd
 import numpy as np
 
 
-df_apt1 = pd.read_excel('https://github.com/jaiwon880/ML_Projiect/raw/main/Data/%EC%95%84%ED%8C%8C%ED%8A%B8(%EB%A7%A4%EB%A7%A4)_%EC%8B%A4%EA%B1%B0%EB%9E%98%EA%B0%80_1%EC%9B%94.xlsx', encoding='cp949',engine='openpyxl')
-df_apt2 = pd.read_excel('https://github.com/jaiwon880/ML_Projiect/raw/main/Data/%EC%95%84%ED%8C%8C%ED%8A%B8(%EB%A7%A4%EB%A7%A4)_%EC%8B%A4%EA%B1%B0%EB%9E%98%EA%B0%80_2%EC%9B%94.xlsx', encoding='cp949',engine='openpyxl')
+df_apt1 = pd.read_excel('https://github.com/jaiwon880/ML_Projiect/blob/main/Data/%EC%95%84%ED%8C%8C%ED%8A%B8(%EB%A7%A4%EB%A7%A4)_%EC%8B%A4%EA%B1%B0%EB%9E%98%EA%B0%80_1%EC%9B%94.xlsx',engine='openpyxl')
+df_apt2 = pd.read_excel('https://github.com/jaiwon880/ML_Projiect/blob/main/Data/%EC%95%84%ED%8C%8C%ED%8A%B8(%EB%A7%A4%EB%A7%A4)_%EC%8B%A4%EA%B1%B0%EB%9E%98%EA%B0%80_2%EC%9B%94.xlsx',engine='openpyxl')
 
 # # 사이드바 // 메인에서 df의 값을 받아온다. main -> create_df -> side_bar
 # def side_bar(df) :
