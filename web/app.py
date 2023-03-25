b07a251069bfc48857c71892d1e25a82992103a6
Han Marie <71927533+jaiwon880@users.noreply.github.com>
2023-03-25 14:14:10 +0900

    d

--- web/app.py
+++ web/app.py
@@ -30,8 +30,9 @@ st.sidebar.title('선택해라')
 selected_region = st.sidebar.selectbox("행정구역 선택해라", 'ㅎㅇ'
 )
 
-# 데이터프레임 출력
-st.write(df_apt1)
+response
+# # 데이터프레임 출력
+# st.write(df_apt1)
 
 
 # df_apt1 = pd.read_excel('https://github.com/jaiwon880/ML_Projiect/raw/main/Data/%EC%95%84%ED%8C%8C%ED%8A%B8(%EB%A7%A4%EB%A7%A4)_%EC%8B%A4%EA%B1%B0%EB%9E%98%EA%B0%80_1%EC%9B%94.xlsx')
