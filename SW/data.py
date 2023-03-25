fd90bd5674507730d7453e352745510d19cf313e
SeoSangwon <126433959+tkd8973@users.noreply.github.com>
2023-03-25 14:50:02 +0900

    csv

--- SW/data.py
+++ SW/data.py
@@ -1,9 +1,8 @@
 import pandas as pd
 
 def read_data():
-    df_apt1 = pd.read_excel('https://github.com/tkd8973/ML_Project/raw/main/Data/%EC%95%84%ED%8C%8C%ED%8A%B8(%EB%A7%A4%EB%A7%A4)_%EC%8B%A4%EA%B1%B0%EB%9E%98%EA%B0%80_1%EC%9B%94.xlsx')
-    df_apt1
-    df_apt2 = pd.read_excel('https://github.com/tkd8973/ML_Project/raw/main/Data/%EC%95%84%ED%8C%8C%ED%8A%B8(%EB%A7%A4%EB%A7%A4)_%EC%8B%A4%EA%B1%B0%EB%9E%98%EA%B0%80_1%EC%9B%94.xlsx')
+    df_apt1 = pd.read_csv('https://github.com/jaiwon880/ML_Projiect/raw/main/Data/%EC%95%84%ED%8C%8C%ED%8A%B8_%EB%A7%A4%EB%A7%A4__%EC%8B%A4%EA%B1%B0%EB%9E%98%EA%B0%80_1%EC%9B%94.csv')
+    df_apt2 = pd.read_excel('https://github.com/jaiwon880/ML_Projiect/blob/main/Data/%EC%95%84%ED%8C%8C%ED%8A%B8_%EB%A7%A4%EB%A7%A4__%EC%8B%A4%EA%B1%B0%EB%9E%98%EA%B0%80_2%EC%9B%94.csv')
     df_apt2
 
     return df_apt1,df_apt2
