649f668823cdd3ef294275d6b2a69b842dba7043
SeoSangwon <126433959+tkd8973@users.noreply.github.com>
2023-03-25 20:15:42 +0900

    test

--- SW/streamlit_p2.py
+++ SW/streamlit_p2.py
@@ -6,13 +6,13 @@ import data
 df_apt1,df_apt2 = data.read_data()
 
 # 사이드바 // 메인에서 df의 값을 받아온다. main -> create_df -> side_bar
-def side_bar(df) :
+def side_bar(df1,df2) :
     s_bar = st.sidebar
     s_bar.title('지역을 선택해주세요.')
 
     # area에 df에서 열 중에서 중복된 값들을 제외하고 리스트로 변환
-
     # 시/도 선택
+
     city_list = df['시군구'].apply(lambda x: x.split()[0]).unique()
     city_choice = s_bar.selectbox('시/도 선택', city_list)
 
@@ -23,21 +23,26 @@ def side_bar(df) :
     # 읍/면/동 선택
     town_list = df['시군구'][df['시군구'].str.contains(city_choice) & df['시군구'].str.contains(gu_choice)].apply(lambda x: x.split()[2]).unique()
     town_choice = s_bar.selectbox('읍/면/동 선택', town_list)
-
+    
     # 리 선택
     try :
         village_list = df['시군구'][df['시군구'].str.contains(city_choice) & df['시군구'].str.contains(gu_choice) & df['시군구'].str.contains(town_choice)].apply(lambda x: x.split()[3]).unique()
         village_choice = s_bar.selectbox('리 선택', village_list)
-        result = df[df['시군구'].str.contains(city_choice) & df['시군구'].str.contains(gu_choice) & df['시군구'].str.contains(town_choice) & df['시군구'].str.contains(village_choice)]
+        Jan_result = df1[df1['시군구'].str.contains(city_choice) & df1['시군구'].str.contains(gu_choice) & df1['시군구'].str.contains(town_choice) & df1['시군구'].str.contains(village_choice)]
+        Feb_result = df2[df2['시군구'].str.contains(city_choice) & df2['시군구'].str.contains(gu_choice) & df2['시군구'].str.contains(town_choice) & df2['시군구'].str.contains(village_choice)]
     except :
-        result = df[df['시군구'].str.contains(city_choice) & df['시군구'].str.contains(gu_choice) & df['시군구'].str.contains(town_choice)]
+        Jan_result = df1[df1['시군구'].str.contains(city_choice) & df1['시군구'].str.contains(gu_choice) & df1['시군구'].str.contains(town_choice)]
+        Feb_result = df2[df2['시군구'].str.contains(city_choice) & df2['시군구'].str.contains(gu_choice) & df2['시군구'].str.contains(town_choice) & df2['시군구'].str.contains(village_choice)]
+
+    
     # 선택된 지역의 데이터만 추출
     
     # result 데이터프레임의 인덱스를 0->1부터 시작하도록 변경
+    result = pd.concat(Jan_result,Feb_result,axis=1)
     result.index = np.arange(1, len(result) + 1)
 
     st.dataframe(result)
 
-    return result
+    return Jan_result, Feb_result
 
-side_bar(df_apt1)
+df1,df2 = side_bar(df_apt1,df_apt2)
