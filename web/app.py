a448802e1e0ae40d56b1d53200c4b56911af5f10
Han Marie <71927533+jaiwon880@users.noreply.github.com>
2023-03-25 13:47:15 +0900

    Rjwuwpqkf

--- web/app.py
+++ web/app.py
@@ -2,7 +2,6 @@ import requests
 import streamlit as st
 import pandas as pd
 import numpy as np
-import xgboost as xgb
 
 df_apt1 = pd.read_excel('https://github.com/jaiwon880/ML_Projiect/raw/main/Data/%EC%95%84%ED%8C%8C%ED%8A%B8(%EB%A7%A4%EB%A7%A4)_%EC%8B%A4%EA%B1%B0%EB%9E%98%EA%B0%80_1%EC%9B%94.xlsx')
 df_apt1
