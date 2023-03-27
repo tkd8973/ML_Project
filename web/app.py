f32590e3c614f8d03fbb80e97f4cbde5e14eee4a
Han Marie <71927533+jaiwon880@users.noreply.github.com>
2023-03-27 12:18:51 +0900

    up1

--- web/app.py
+++ web/app.py
@@ -20,42 +20,53 @@ st.write(
 
 
 df_apt1 = pd.read_csv('Data/아파트_매매__실거래가_20230321134107_1월.csv')
-
+df_apt2 = pd.read_csv('Data/아파트_매매__실거래가_20230321134120_2월.csv')
 
 df_apt1["거래금액(만원)"] = df_apt1["거래금액(만원)"].str.replace(",", "").astype(int) # 거래금액 컬럼의 쉼표를 제거하고 정수형으로 변환
-df_apt1["평당가"] = (df_apt1["거래금액(만원)"] / df_apt1["전용면적(㎡)"]) / 3.3 # 평당가 계산하여 새로운 컬럼 추가
+df_apt1["평단가"] = (df_apt1["거래금액(만원)"] / df_apt1["전용면적(㎡)"]) / 3.3 # 평단가 계산하여 새로운 컬럼 추가
 df_apt1
 
 
-# 사이드바 
-def side_bar(df_apt1) :
-  s_bar = st.sidebar
-  s_bar.title('지역을 선택해주세요.')
+df_apt1,df_apt2 = data.read_data()
 
-  # 시/도 선택
-  city_list = df_apt1['시군구'].apply(lambda x: x.split()[0]).unique()
-  city_choice = s_bar.selectbox('시/도 선택', city_list)
+# 사이드바 // 메인에서 df의 값을 받아온다. main -> create_df -> side_bar
+def side_bar(df1,df2) :
+    s_bar = st.sidebar
+    s_bar.title('지역을 선택해주세요.')
 
-  # 시/군/구 선택
-  gu_list = df_apt1['시군구'][df_apt1['시군구'].str.contains(city_choice)].apply(lambda x: x.split()[1]).unique()
-  gu_choice = s_bar.selectbox('시/군/구 선택', gu_list)
+    # area에 df에서 열 중에서 중복된 값들을 제외하고 리스트로 변환
+    # 시/도 선택
 
-  # 읍/면/동 선택
-  town_list = df_apt1['시군구'][df_apt1['시군구'].str.contains(city_choice) & df_apt1['시군구'].str.contains(gu_choice)].apply(lambda x: x.split()[2]).unique()
-  town_choice = s_bar.selectbox('읍/면/동 선택', town_list)
+    city_list = df1['시군구'].apply(lambda x: x.split()[0]).unique()
+    city_choice = s_bar.selectbox('시/도 선택', city_list)
 
-  # 리 선택
-  village_list = df_apt1['시군구'][df_apt1['시군구'].str.contains(city_choice) & df_apt1['시군구'].str.contains(gu_choice) & df['시군구'].str.contains(town_choice)].apply(lambda x: x.split()[3]).unique()
-  village_choice = s_bar.selectbox('리 선택', village_list)
+    # 시/군/구 선택
+    gu_list = df1['시군구'][df1['시군구'].str.contains(city_choice)].apply(lambda x: x.split()[1]).unique()
+    gu_choice = s_bar.selectbox('시/군/구 선택', gu_list)
 
-  # 선택된 지역의 데이터만 추출
-  result = df_apt1[df_apt1['시군구'].str.contains(city_choice) & df_apt1['시군구'].str.contains(gu_choice) & df_apt1['시군구'].str.contains(town_choice) & df['시군구'].str.contains(village_choice)]
+    # 읍/면/동 선택
+    town_list = df1['시군구'][df1['시군구'].str.contains(city_choice) & df1['시군구'].str.contains(gu_choice)].apply(lambda x: x.split()[2]).unique()
+    town_choice = s_bar.selectbox('읍/면/동 선택', town_list)
+    
+    # 리 선택
+    try :
+        village_list = df1['시군구'][df1['시군구'].str.contains(city_choice) & df1['시군구'].str.contains(gu_choice) & df1['시군구'].str.contains(town_choice)].apply(lambda x: x.split()[3]).unique()
+        village_choice = s_bar.selectbox('리 선택', village_list)
+        Jan_result = df1[df1['시군구'].str.contains(city_choice) & df1['시군구'].str.contains(gu_choice) & df1['시군구'].str.contains(town_choice) & df1['시군구'].str.contains(village_choice)]
+        Feb_result = df2[df2['시군구'].str.contains(city_choice) & df2['시군구'].str.contains(gu_choice) & df2['시군구'].str.contains(town_choice) & df2['시군구'].str.contains(village_choice)]
+    except :
+        Jan_result = df1[df1['시군구'].str.contains(city_choice) & df1['시군구'].str.contains(gu_choice) & df1['시군구'].str.contains(town_choice)]
+        Feb_result = df2[df2['시군구'].str.contains(city_choice) & df2['시군구'].str.contains(gu_choice) & df2['시군구'].str.contains(town_choice)]
 
-  # result 데이터프레임의 인덱스를 0->1부터 시작하도록 변경
-  result.index = np.arange(1, len(result) + 1)
+    
+    # 선택된 지역의 데이터만 추출
+    
+    # result 데이터프레임의 인덱스를 0->1부터 시작하도록 변경
+    Jan_result.index = np.arange(1,len(Jan_result) + 1)
+    Feb_result.index = np.arange(1,len(Feb_result) + 1)
+    result = pd.concat([Jan_result,Feb_result])
+    st.dataframe(result)
 
-  return result
+    return Jan_result, Feb_result
 
-
-# df_apt2 = pd.read_csv('Data/아파트_매매__실거래가_20230321134120_2월.csv')
-# df_apt2
+df1,df2 = side_bar(df_apt1,df_apt2)
