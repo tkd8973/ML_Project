2d496b12559d2c6f9d53519c0d82630bd80c9c12
SeoSangwon <126433959+tkd8973@users.noreply.github.com>
2023-03-28 17:34:34 +0900

    df

--- SW/review/app.py
+++ SW/review/app.py
@@ -37,7 +37,7 @@ def sidebar() :
 def contents():
     # st.write(get_filtered_data())
     # st.write(handle_preprocessing())
-    tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["Linear Regressor", 'KNN', "Decision Tree", 'Random Forest', "XGBoost", "LightGBM"])
+    tab0, tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(['df',"Linear Regressor", 'KNN', "Decision Tree", 'Random Forest', "XGBoost", "LightGBM"])
     with tab0:
         background()
     with tab1: 
