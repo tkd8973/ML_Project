e12f35d59c2598c9592ee63ffdfd804306c17656
SeoSangwon <126433959+tkd8973@users.noreply.github.com>
2023-03-28 14:44:32 +0900

    ㅐㄴ

--- SW/review/app.py
+++ SW/review/app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import glob
+import os
+
 def main():
     with st.sidebar: sidebar()
     contents()
@@ -7,6 +9,9 @@ def main():
 def sidebar() :
     title =  '지역을 선택해주세요.'
     st.title(title)
+    print(__file__)
+    print(os.path.realpath(__file__))
+    print(os.path.abspath(__file__))
     st.write(glob.glob('static/*.csv'))
 
 def contents():
