8d77ac2456fb172523e769ea87f2d72dd6eb3a07
SeoSangwon <126433959+tkd8973@users.noreply.github.com>
2023-03-28 14:57:43 +0900

    gu_list

--- SW/review/app.py
+++ SW/review/app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from data import get_city_list
+from data import get_city_list, get_gu_list
 
 def main():
     with st.sidebar: sidebar()
@@ -9,8 +9,9 @@ def sidebar() :
     title =  '지역을 선택해주세요.'
     st.title(title)
     label_city_list = '시/도 선택'
-    st.selectbox(label_city_list, get_city_list())
-    
+    city_choice = st.selectbox(label_city_list, get_city_list())
+    st.selectbox(label_city_list, get_gu_list(city_choice))
+
 
 def contents():
     pass
--- SW/review/data.py
+++ SW/review/data.py
@@ -7,7 +7,15 @@ def read_data():
     glob_pt = '/app/ml_project/SW/review/static/*.csv'
     return pd.concat([pd.read_csv(p) for p in sorted(glob.glob(glob_pt))])  
 
+def get_unique_list(data, index):
+    return data.str.split(expand=True)[index].unique()
+
 @st.cache_data
 def get_city_list():
     df = read_data()
-    return df['시군구'].str.split(expand=True)[0].unique()
+    return get_unique_list(df['시군구'], 0)
+
+@st.cache_data
+def get_gu_list(city_choice):
+    df = read_data()
+    return get_unique_list(df['시군구'][df['시군구'].str.contains(city_choice)], 1)
