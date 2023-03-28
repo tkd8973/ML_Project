bc711871be85e32f4fc08c6343ce20fa8be2e486
SeoSangwon <126433959+tkd8973@users.noreply.github.com>
2023-03-28 12:07:22 +0900

    dsa

--- SW/streamlit_p2.py
+++ SW/streamlit_p2.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import data
-from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsRegressor
 
