d19820fa61c593a4ef413a4e66db62ac1756d7ac
SeoSangwon <126433959+tkd8973@users.noreply.github.com>
2023-03-28 17:24:32 +0900

    ssor

--- SW/review/app.py
+++ SW/review/app.py
@@ -7,7 +7,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 import xgboost as xgb
 import lightgbm as lgbionTreeRegressor
-from xgboost import XGBRegress
+from xgboost import XGBRegressor
 from data import get_city_list, get_gu_list, get_town_list, get_village_list
 from service import get_filtered_data, handle_preprocessing
 
