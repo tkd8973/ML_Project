44c6c4af27f9ee9b066e8514dc30a5d51209aac3
SeoSangwon <126433959+tkd8973@users.noreply.github.com>
2023-03-28 14:49:06 +0900

    folder

--- SW/review/app.py
+++ SW/review/app.py
@@ -1,7 +1,5 @@
 import streamlit as st
-import glob
-import os
-import pandas as pd 
+from data import read_data
 
 def main():
     with st.sidebar: sidebar()
@@ -10,8 +8,8 @@ def main():
 def sidebar() :
     title =  '지역을 선택해주세요.'
     st.title(title)
-    df = pd.concat([pd.read_csv(p) for p in sorted(glob.glob('/app/ml_project/SW/review/static/*.csv'))])    
-    st.write(df)
+      
+    st.write(read_data())
 
 def contents():
     pass
