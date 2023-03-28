b6d92d1130497e0fdf1ba439681e1fc0bb56c016
SeoSangwon <126433959+tkd8973@users.noreply.github.com>
2023-03-28 10:24:44 +0900

    aa

--- SW/streamlit_p2.py
+++ SW/streamlit_p2.py
@@ -38,11 +38,12 @@ def side_bar(df1) :
     # result 데이터프레임의 인덱스를 0->1부터 시작하도록 변경
     Jan_result.index = np.arange(1,len(Jan_result) + 1)
     
-    st.dataframe(Jan_result)
+
 
     return Jan_result
 df_apt1 = side_bar(df_apt1)
 columns = df_apt1.columns
-pd.concat([df_apt9[columns],df_apt10[columns],\
+result = pd.concat([df_apt9[columns],df_apt10[columns],\
 df_apt11[columns],df_apt12[columns],df_apt1[columns],df_apt2[columns]])
 
+st.dataframe(result)
