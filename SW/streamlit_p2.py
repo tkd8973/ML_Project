e504919b2bcf7030be279bf4ba12857822bf644c
SeoSangwon <126433959+tkd8973@users.noreply.github.com>
2023-03-28 12:17:04 +0900

    x

--- SW/streamlit_p2.py
+++ SW/streamlit_p2.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import data
+import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsRegressor
 
