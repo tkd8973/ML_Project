9de2244a4a2ab21f5b521c8aea0f4ffda2b60164
SeoSangwon <126433959+tkd8973@users.noreply.github.com>
2023-03-29 18:37:30 +0900

    sa

--- SW/review/app.py
+++ SW/review/app.py
@@ -144,18 +144,24 @@ def lr():
 
 # knn 모델
 def knn():
-    X_train,y_train,X_test,y_test = load_data()
-    
-    models = []
+    # 데이터 로드
+    X_train, y_train, X_test, y_test = load_data()
+    # 모델 훈련
+    param_grid = {'n_neighbors': [3, 5, 7, 9],
+                  'weights': ['uniform', 'distance'],
+                  'p': [1, 2, 3]}
     model = KNeighborsRegressor()
-    model.fit(X_train,y_train)
-
-    pred=model.predict(X_test)
-    rmse = mean_squared_error(y_test,pred)**0.5
-
-    st.write(result)
-    
-    return model
+    grid_search = GridSearchCV(model, param_grid=param_grid, cv=5, n_jobs=-1)
+    grid_search.fit(X_train, y_train)
+    # 결과 출력
+    results = pd.DataFrame(grid_search.cv_results_)
+    st.write(results)
+    # fig = go.Figure(data=go.Surface(x=results['param_n_neighbors'], y=results['param_p'], z=results['mean_test_score'].values.reshape(4, 3)))
+    fig.update_layout(title="KNN Model Performance",
+                      scene=dict(xaxis_title="Number of neighbors",
+                                 yaxis_title="Power parameter (p)",
+                                 zaxis_title="Mean test score"))
+    st.plotly_chart(fig)
 
 # 랜덤포레스트 모델
 def rdf():
