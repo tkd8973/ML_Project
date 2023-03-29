1216b1d53f247c6d8079c63bfdb65e733f6e18c4
SeoSangwon <126433959+tkd8973@users.noreply.github.com>
2023-03-29 14:37:20 +0900

    d

--- SW/review/app.py
+++ SW/review/app.py
@@ -152,7 +152,7 @@ def knn():
     grid_search = GridSearchCV(model, param_grid=param_grid, cv=5, n_jobs=-1)
     grid_search.fit(X_train, y_train)
     mean_test_scores = grid_search.cv_results_['mean_test_score']
-    fig = px.line(x=len(mean_test),y=mean_test_scores.reshape(1,-1))
+    fig = px.line(x=len(mean_test_scores),y=mean_test_scores.reshape(1,-1))
     fig.show()
     st.plotly(fig)
     st.write(mean_test_scores)
