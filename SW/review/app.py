4e63f845ec05b80443aa8b5115795726dc65b9f1
SeoSangwon <126433959+tkd8973@users.noreply.github.com>
2023-03-29 15:37:23 +0900

    wd

--- SW/review/app.py
+++ SW/review/app.py
@@ -160,7 +160,7 @@ def knn():
 
     result = np.array(models).reshape(1,-1)
 
-    px.line(x=range(0,6),y=result)
+    px.line(x=range(1,6),y=result)
     st.plotly_chart(fig)
     
     return model
