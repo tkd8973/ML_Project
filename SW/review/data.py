2f3fa16cac14acf27be7ced5128d0f2dfac860c2
SeoSangwon <126433959+tkd8973@users.noreply.github.com>
2023-03-29 19:35:41 +0900

    tset

--- SW/review/data.py
+++ SW/review/data.py
@@ -4,7 +4,7 @@ import glob
 
 @st.cache_data
 def read_data():
-    glob_pt = '/app/ml_project/SW/review/static/*.csv'
+    glob_pt = 'SW/review/static/*.csv'
     return pd.concat([pd.read_csv(p) for p in sorted(glob.glob(glob_pt))])  
 
 def get_unique_list(data, index):
